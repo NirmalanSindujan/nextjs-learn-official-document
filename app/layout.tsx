2b54e899dd29d85b9c47d6c2a7ffbba3a2d7c83a
Sindujan <sindujan@paraqum.com>
2024-01-02 15:16:32 +0530

    mutation issue fixed

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,15 @@
 import '@/app/ui/global.css'; 
 import { inter } from '@/app/ui/fonts';
+import { Poppins } from 'next/font/google';
+import { lusitana } from '@/app/ui/fonts';
+
+const poppins = Poppins({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-poppins',
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900']
+});
+
 
 export default function RootLayout({
   children,
@@ -8,7 +18,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-        <body className={`${inter.className} antialiased`}>{children}</body>
+        <body className={`${poppins.variable} antialiased`}>{children}</body>
     </html>
   );
 }
